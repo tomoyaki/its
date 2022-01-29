15e6dc6b4c2800f1303336a7d892d4b83ae53167
tomoyaki <tomoyaki74@gmail.com>
2022-01-29 19:33:29 +0900

    send values to serverside

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,7 +28,7 @@ public class IssueController {
 
     // GET /issues/creationForm
     @GetMapping("creationForm")
-    public String showCreationForm() {
+    public String showCreationForm(@ModelAttribute IssueForm form) {
         return "issues/creationForm";
     }
 
