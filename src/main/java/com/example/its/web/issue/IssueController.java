6d930f953c60157dfc2350b25ab115bc0e403e72
tomoyaki <tomoyaki74@gmail.com>
2022-01-29 19:04:22 +0900

    implement creation form

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,20 +6,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
     // GET /issuesに対応するメソッド
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    // GET /issues/creationForm
+    @GetMapping("creationForm")
+    public String showCreationForm() {
+        return "issues/creationForm";
     }
 
 }
