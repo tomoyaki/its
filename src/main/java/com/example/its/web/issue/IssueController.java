b38e01e43bfcf9d5fe342467df815a2f6fe7177d
tomoyaki <tomoyaki74@gmail.com>
2022-01-29 20:05:45 +0900

    use PRG pattern

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -36,7 +36,7 @@ public class IssueController {
     @PostMapping
     public String create(IssueForm form, Model model) {
         issueService.create(form.getSummary(), form.getDescription());
-        return showList(model); // TODO リロードボタン対策が必要
+        return "redirect:/issues";
     }
 
 }
